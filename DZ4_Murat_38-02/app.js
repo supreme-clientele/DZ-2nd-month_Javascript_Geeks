581d7a67bcd1602b6eea39c56ea47a0ad1017c06
Murat Bukarbaev <85822522+supreme-clientele@users.noreply.github.com>
2024-05-18 06:38:00 -0400

    DZ4

---
+++ DZ4_Murat_38-02/app.js
@@ -0,0 +1,33 @@
+// функция капитализации строк
+function capitalizeString(s) {
+    if (s.length === 0) {
+        return "";
+    }
+    let firstChar = s[0].toUpperCase();
+    let restOfString = s.substring(1).toLowerCase();
+    return firstChar + restOfString;
+}
+
+console.log(capitalizeString('MURAT'));
+console.log(capitalizeString('bUKARBAEV'));
+
+
+// функция подсчета количества определенных символов в строке
+function charCount(str, char) {
+    let lowerStr = str.toLowerCase();
+    let lowerChar = char.toLowerCase();
+    
+    let count = 0;
+    
+    for (let i = 0; i < lowerStr.length; i++) {
+        if (lowerStr[i] === lowerChar) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+console.log(charCount('Abrakadabra', 'a'));
+console.log(charCount('pneumonia', 'n'));
+console.log(charCount('Essential', 'n'));
