1fd91a7130f548bd5f86495966c7d2c197db2323
Murat Bukarbaev <murat.bukarbaev@gmail.com>
2024-05-22 16:22:36 +0600

    DZ5 Update

--- DZ5_Murat_38-02/app.js
+++ DZ5_Murat_38-02/app.js
@@ -1,34 +1,34 @@
 let count = 0;
-        const counterElement = document.getElementById('counter');
-        const increaseButton = document.getElementById('increase');
-        const resetButton = document.getElementById('reset');
-        const decreaseButton = document.getElementById('decrease');
+const counterElement = document.getElementById('counter');
+const increaseButton = document.getElementById('increase');
+const resetButton = document.getElementById('reset');
+const decreaseButton = document.getElementById('decrease');
 
-        function updateCounter() {
-            counterElement.textContent = count;
+function updateCounter() {
+    counterElement.textContent = count;
 
-            if (count > 0) {
-                counterElement.style.color = 'green';
-            } else if (count < 0) {
-                counterElement.style.color = 'red';
-            } else {
-                counterElement.style.color = 'gray';
-            }
-        }
+    if (count > 0) {
+        counterElement.style.color = 'green';
+    } else if (count < 0) {
+        counterElement.style.color = 'red';
+    } else {
+        counterElement.style.color = 'gray';
+    }
+}
 
-        increaseButton.addEventListener('click', function() {
-            count += 1;
-            updateCounter();
-        });
+increaseButton.addEventListener('click', function() {
+    count += 1;
+    updateCounter();
+});
 
-        resetButton.addEventListener('click', function() {
-            count = 0;
-            updateCounter();
-        });
+resetButton.addEventListener('click', function() {
+    count = 0;
+    updateCounter();
+});
 
-        decreaseButton.addEventListener('click', function() {
-            count -= 1;
-            updateCounter();
-        });
+decreaseButton.addEventListener('click', function() {
+    count -= 1;
+    updateCounter();
+});
 
-        updateCounter();
+updateCounter();
